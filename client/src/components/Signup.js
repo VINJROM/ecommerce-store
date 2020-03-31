4e95d9445562eb677519bf966c519eec68326b20
Vincent Romine <vinjrom@live.com>
2020-03-30 22:36:44 -0500

    add activeClassName styles

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -6,4 +6,4 @@ class Signup extends React.Component {
   }
 }
 
-export default Signup
+export default Signup;
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "./index.css";
+import "gestalt/dist/gestalt.css";
 
 import App from "./components/App";
+import Navbar from "./components/Navbar";
 import Signin from "./components/Signin";
 import Signup from "./components/Signup";
 import Checkout from "./components/Checkout";
@@ -12,14 +13,21 @@ import registerServiceWorker from "./registerServiceWorker";
 
 const Root = () => (
   <Router>
-    <Switch>
-      <Route component={App} exact path="/" />
-      <Route component={Signin} path="/signin" />
-      <Route component={Signup} path="/signup" />
-      <Route component={Checkout} path="/checkout" />
-    </Switch>
+    <React.Fragment>
+      <Navbar />
+      <Switch>
+        <Route component={App} exact path="/" />
+        <Route component={Signin} path="/signin" />
+        <Route component={Signup} path="/signup" />
+        <Route component={Checkout} path="/checkout" />
+      </Switch>
+    </React.Fragment>
   </Router>
-)
+);
 
 ReactDOM.render(<Root />, document.getElementById("root"));
 registerServiceWorker();
+
+if (module.hot) {
+  module.hot.accept();
+}
