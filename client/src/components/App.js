10083e62a78cf7e01727e1cc374a9ac58613c774
Vincent Romine <vinjrom@live.com>
2020-04-02 19:00:05 -0500

    setup server requests

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,27 +2,35 @@ import React, { Component } from "react";
 import { Container, Box, Heading } from "gestalt";
 import "./App.css";
 import Strapi from "strapi-sdk-javascript/build/main";
-const apiUrl = process.env.API_URL || "http://localhost:1337/";
+const apiUrl = process.env.API_URL || "http://localhost:1337";
 const strapi = new Strapi(apiUrl);
 
 class App extends Component {
+  state = {
+    brands: []
+  };
+
   async componentDidMount() {
-    const response = await strapi.request("POST", "/graphql", {
-      data: {
-        query: `
-        query {
-          brands {
-            _id
-            name
-            description
-            image{
+    try {
+      const response = await strapi.request("POST", "/graphql", {
+        data: {
+          query: `query {
+            brands {
+              _id
               name
+              description
+              image {
+                url
+              }
             }
-          }
-        }`
-      }
-    });
-    console.log(response);
+          }`
+        }
+      });
+      console.log(response);
+      this.setState({ brands: response.data.brands });
+    } catch (err) {
+      console.error(err);
+    }
   }
 
   render() {
