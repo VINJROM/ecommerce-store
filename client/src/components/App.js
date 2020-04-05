dfa11c42c2161268c7218f0ba33558f6a22a5b42
Vincent Romine <vinjrom@live.com>
2020-04-04 23:32:55 -0500

    Revert "setup server requests"
    This reverts commit 10083e62a78cf7e01727e1cc374a9ac58613c774.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,35 +2,27 @@ import React, { Component } from "react";
 import { Container, Box, Heading } from "gestalt";
 import "./App.css";
 import Strapi from "strapi-sdk-javascript/build/main";
-const apiUrl = process.env.API_URL || "http://localhost:1337";
+const apiUrl = process.env.API_URL || "http://localhost:1337/";
 const strapi = new Strapi(apiUrl);
 
 class App extends Component {
-  state = {
-    brands: []
-  };
-
   async componentDidMount() {
-    try {
-      const response = await strapi.request("POST", "/graphql", {
-        data: {
-          query: `query {
-            brands {
-              _id
+    const response = await strapi.request("POST", "/graphql", {
+      data: {
+        query: `
+        query {
+          brands {
+            _id
+            name
+            description
+            image{
               name
-              description
-              image {
-                url
-              }
             }
-          }`
-        }
-      });
-      console.log(response);
-      this.setState({ brands: response.data.brands });
-    } catch (err) {
-      console.error(err);
-    }
+          }
+        }`
+      }
+    });
+    console.log(response);
   }
 
   render() {
