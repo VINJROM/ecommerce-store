c42b4de185fa1ba0e416794565d04977fc8d174b
Vincent Romine <vinjrom@live.com>
2020-04-22 22:16:16 -0500

    ok...

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Box, Text, Heading, Image, Button } from "gestalt";
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { getToken, clearToken, clearCart } from "../utils";
 import { NavLink, withRouter } from "react-router-dom";
 
@@ -31,6 +32,14 @@ const Navbar = () => {
 =======
 import { NavLink } from "react-router-dom";
 import { getToken } from "../utils";
+=======
+import { NavLink } from "react-router-dom";
+import { getToken } from "../utils";
+
+const Navbar = () => {
+  return getToken() !== null ? <AuthNav /> : <UnAuthNav />;
+};
+>>>>>>> parent of f07961f... create clear token, clear cart, and sign in auth
 
 const Navbar = () => {
   return getToken() !== null ? <AuthNav /> : <UnAuthNav />;
